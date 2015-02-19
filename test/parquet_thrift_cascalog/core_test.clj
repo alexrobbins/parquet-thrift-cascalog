c9ea4f59ada918b2df6301bb6fd9bc4a38d4fde1
Alex Robbins <alexander.j.robbins@gmail.com>
2015-02-19 17:16:41 +0000

    Projection tests working.

--- test/parquet_thrift_cascalog/core_test.clj
+++ test/parquet_thrift_cascalog/core_test.clj
@@ -15,31 +15,34 @@
     (let [name (doto (Name. 1 "First name")
                  (.setLast_name "Last name"))]
       (io/with-fs-tmp [_ tmp]
-        (?- (hfs-parquet tmp :thrift-class Name)   ;; write line
+        (?- (hfs-parquet tmp :thrift-class Name)   ;; write names
             [name])
         (test?<- [name]
                  [?name]
                  ((hfs-parquet tmp) ?name))))))
 
+(defn make-name [id fname & [lname]]
+  (let [name (Name. id fname)]
+    (when lname
+      (.setLast_name name lname))
+    name))
+
 (defn make-names [& names]
-  (mapv (fn [[id fname lname]]
-          (let [name (Name. id fname)]
-            (when lname
-              (.setLast_name name lname))
-            name))
-        names))
+  (mapv #(apply make-name %) names))
+
+(def names
+  (make-names
+   [1 "A" "Lastname"]
+   [2 "B" "Lastname"]
+   [3 "C" nil]))
 
 (deftest filter-test
   (io/with-log-level :fatal
     (let [id-pred (FilterApi/eq (FilterApi/intColumn "id") (int 1)) ;; without coercion this fails as a long.
           string-pred (FilterApi/eq (FilterApi/binaryColumn "first_name") (Binary/fromString "A"))
-          nil-pred (FilterApi/eq (FilterApi/binaryColumn "last_name") nil)
-          names (make-names
-                 [1 "A" "Lastname"]
-                 [2 "B" "Lastname"]
-                 [3 "C" nil])]
+          nil-pred (FilterApi/eq (FilterApi/binaryColumn "last_name") nil)]
       (io/with-fs-tmp [_ tmp]
-        (?- (hfs-parquet tmp :thrift-class Name)   ;; write line
+        (?- (hfs-parquet tmp :thrift-class Name)   ;; write names
             names)
         (test?<- [(first names)]
                  [?name]
@@ -50,3 +53,18 @@
         (test?<- [(last names)]
                  [?name]
                  ((hfs-parquet tmp :filter nil-pred) ?name))))))
+
+(deftest projection-test
+  (io/with-log-level :fatal
+    (io/with-fs-tmp [_ tmp]
+      (?- (hfs-parquet tmp :thrift-class Name)   ;; write names
+          names)
+      (test?<- (make-names [1 "A"] [2 "B"] [3 "C"])
+               [?name]
+               ((hfs-parquet tmp :projection "id;first_name") ?name))
+      (test?<- (make-names [1 ""] [2 ""] [3 ""])
+               [?name]
+               ((hfs-parquet tmp :projection "id") ?name))
+      (test?<- (make-names [0 "" "Lastname"] [0 "" "Lastname"] [0 ""])
+               [?name]
+               ((hfs-parquet tmp :projection "last_name") ?name)))))
