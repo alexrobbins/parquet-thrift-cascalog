fcde39a21d57fc16f5380931450e9f4fb2c97cf9
Alex Robbins <alexander.j.robbins@gmail.com>
2015-02-19 21:14:41 +0000

    0.1.0 release.

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject parquet-thrift-cascalog "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+(defproject parquet-thrift-cascalog "0.1.0"
+  :description "Idiomatic Cascalog taps for reading Thrift-specified Parquet data."
+  :url "https://github.com/alexrobbins/parquet-thrift-cascalog"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :plugins [[lein-thriftc "0.2.1"]
